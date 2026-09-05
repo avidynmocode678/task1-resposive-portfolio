16fe22523c580feccf7ae04424437854557613c7
avidynmocode678 <abhishekgiri5215@gmail.com>
2026-09-05 18:46:50 +0530

    Add files via upload
    task 1 resposive portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+// Contact form message
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    alert("Thank you! Your message has been received.");
+
+    this.reset();
+});
